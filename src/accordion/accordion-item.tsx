8bd545e3fe0dfd3fa8cb87d1c1804a4d3d323642
Richard George <richard.jhn.george.3@gmail.com>
2023-03-16 11:37:10 +0000

    fixed spacing in menu and removed description -

--- src/accordion/accordion-item.tsx
+++ src/accordion/accordion-item.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import "./styles.css";
 
 import { useState } from "react";
@@ -9,12 +7,6 @@ import type { Item, SubItem } from "./accordion.types";
 interface AccordionItemProps {
   item: Item;
 }
-
-//const GradiantOverlay: CSS.Properties =   {
-//  background: 'linear-gradient(180deg, rgba(2, 0, 36, 0) 0%, #000 100%)',
-//  border-radius: '15px',
-//  height: '100%',
-//};
 
 export const AccordionItem = ({
   item,
@@ -37,9 +29,6 @@ export const AccordionItem = ({
         <p className={"accordion-tropdown-text"}>
           <a href={subItem.url}>{subItem.title.substring(0, 15)}</a>
         </p>
-        <p className={"accordion-dropdown-text"}>
-          {subItem.description.substring(0, 30)}
-        </p>
       </div>
     );
   };
@@ -49,38 +38,30 @@ export const AccordionItem = ({
   };
 
   return (
-    <>
-      <div>
-        {item.url ? (
-          <div className="accordion-item-container">
-            <p className={"accordion-dropdown-text"}>
-              <a href={item.url}>{item.title.substring(0, 15)}</a>
-            </p>
-            <p className={"accordion-dropdown-text"}>
-              {item.description?.substring(0, 30)}
-            </p>
+    <div>
+      {item.url ? (
+        <div className="accordion-item-container">
+          <p className={"accordion-dropdown-text"}>
+            <a href={item.url}>{item.title.substring(0, 15)}</a>
+          </p>
+        </div>
+      ) : (
+        <div className="accordion-item-container">
+          <p className={"accordion-dropdown-text"}>{item.title.substring(0, 15)}</p>
+          <div className={"accordian-button"} onClick={toggleVisible}>
+            {isVisible ? "▲" : "▼"}
           </div>
-        ) : (
-          <div className="accordion-item-container">
-            <p className={"accordion-dropdown-text"}>{item.title.substring(0, 15)}</p>
-            <p className={"accordion-dropdown-text"}>
-              {item.description?.substring(0, 30)}
-            </p>
-            <div className={"accordian-button"} onClick={toggleVisible}>
-              {isVisible ? "▲" : "▼"}
-            </div>
+        </div>
+      )}
+      <div>
+        {isVisible && item.subItems && (
+          <div>
+            {item.subItems.map((subItem, index) =>
+              renderSubMenu(subItem, index)
+            )}
           </div>
         )}
-        <div>
-          {isVisible && item.subItems && (
-            <div>
-              {item.subItems.map((subItem, index) =>
-                renderSubMenu(subItem, index)
-              )}
-            </div>
-          )}
-        </div>
       </div>
-    </>
+    </div>
   );
 };
